681ab651e6a03d18dff593697e5b245edee3b1f0
mfaizk <faizdev@chainbull.net>
2026-02-19 16:56:20 +0530

    package updagte

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,9 @@
-import { PayButtonProps } from "./src/index.ts";
+import { PayButton } from "./src/index.ts";
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      "pay-button": PayButtonProps;
+      "pay-button": PayButton;
     }
   }
 }
