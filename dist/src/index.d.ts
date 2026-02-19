dd8f9e19a096e3c7af1200172ae16a27f76fbf28
mfaizk <faizdev@chainbull.net>
2026-02-19 16:43:48 +0530

    package updagte

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,3 +1,2 @@
-export { PayButton } from "./pay-button/index.js";
-export type { PayButtonProps } from "./pay-button/index.js";
+export * from "./pay-button/index.js";
 //# sourceMappingURL=index.d.ts.map
--- dist/src/index.js
+++ dist/src/index.js
@@ -1,2 +1,4 @@
-export { PayButton } from "./pay-button/index.js";
+// export  { PayButton } from "./pay-button/index.js";
+// export type { PayButtonProps } from "./pay-button/index.js";
+export * from "./pay-button/index.js";
 //# sourceMappingURL=index.js.map
